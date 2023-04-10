f12857ea419b4667a9d2fbecc8547d31e1a1009b
clapscofield <clarisse.scofield@gmail.com>
2023-04-10 15:31:34 -0300

    DAy 2 - Part 2

--- day2/solution.py
+++ day2/solution.py
@@ -20,6 +20,22 @@ def rockPaperScissorsGame(gameLines):
     return total
 
 
+"""PART 2 - Adapted Logic"""
+def rockPaperScissorsGamePart2(gameLines):
+    total = 0
+    for line in gameLines:
+        x, y = line.split()
+
+        x = ord(x) - 65
+        if y == "X":
+            total += (x - 1) % 3 + 1
+        elif y == "Y":
+            total += 3 + x + 1
+        else:
+            total += 6 + (x + 1) % 3 + 1
+    return total
+
+
 SAMPLE_INPUT = [
     "A Y",
     "B X",
@@ -27,7 +43,7 @@ SAMPLE_INPUT = [
 ]
 #Test for the Sample Input
 print(rockPaperScissorsGame(SAMPLE_INPUT))
-
+print(rockPaperScissorsGamePart2(SAMPLE_INPUT))
 
 #Test for the given Input
 # open a file
@@ -35,3 +51,4 @@ inputFile = open("input.txt", "r")
 # read the file
 lines = inputFile.readlines()
 print(rockPaperScissorsGame(lines))
+print(rockPaperScissorsGamePart2(lines))
