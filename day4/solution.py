aba0a547fa54a194b4d4bc1ffeeabfa00af8fc36
clapscofield <clarisse.scofield@gmail.com>
2023-04-10 16:21:45 -0300

    Day 4 - Part 2

--- day4/solution.py
+++ day4/solution.py
@@ -13,13 +13,24 @@ def campCleanup(sections):
     return count
 
 
+"""Part 2"""
+def campCleanupPart2(sections):
+    count = 0
+
+    for line in sections:
+        start1, end1, start2, end2 = map(int, line.replace(",", "-").split("-"))
+        if start1 <= end2 and end1 >= start2:
+            count += 1
+    return count
+
 #Test on sample input
 SAMPLE_INPUT = ["2-4,6-8", "2-3,4-5", "5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8"]
 print(campCleanup(SAMPLE_INPUT))
-
+print(campCleanupPart2(SAMPLE_INPUT))
 
 #Test on given input
 inputFile = open("input.txt", "r")
 # read the file
 lines = inputFile.readlines()
 print(campCleanup(lines))
+print(campCleanupPart2(lines))
