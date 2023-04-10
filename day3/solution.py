91029cfb72f30f8fdb1c194fe686e7618f41ec8a
clapscofield <clarisse.scofield@gmail.com>
2023-04-10 16:10:08 -0300

    Day 3 - Part 2

--- day3/solution.py
+++ day3/solution.py
@@ -10,13 +10,31 @@ def rucksackReorganization(itemsList):
         middle = len(rucksack) // 2 #the middle
         a = rucksack[:middle] #first part
         b = rucksack[middle:] #second part
-        
+
         for item in set(a).intersection(b):
             if "a" <= item <= "z":
                 total += ord(item) - ord("a") + 1
             if "A" <= item <= "Z":
                 total += ord(item) - ord("A") + 27
 
+    return total
+
+"""PART 2"""
+def rucksackReorganizationPart2(itemsList):
+    total = 0
+
+    # group every third element of a list of strings, 
+    # after removing whitespace in each element using a range function.
+    chunk_size = 3
+    chunks = [map(str.strip, itemsList[i:i+chunk_size]) for i in range(0, len(itemsList), chunk_size)]
+    result = list(zip(*chunks))
+
+    for first, second, third in result:
+        item =  set(first).intersection(second).intersection(third).pop()
+        if "a" <= item <= "z":
+                total += ord(item) - ord("a") + 1
+        if "A" <= item <= "Z":
+            total += ord(item) - ord("A") + 27
     return total
 
 #Test on Sample Input
@@ -29,9 +47,11 @@ SAMPLE_INPUT = [
     "CrZsJsPPZsGzwwsLwLmpwMDw",
 ]
 print(rucksackReorganization(SAMPLE_INPUT))
+print(rucksackReorganizationPart2(SAMPLE_INPUT))
 
 #Test on given input
 inputFile = open("input.txt", "r")
 # read the file
 lines = inputFile.readlines()
 print(rucksackReorganization(lines))
+print(rucksackReorganizationPart2(lines))
